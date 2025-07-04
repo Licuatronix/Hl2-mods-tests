9a80056e6163660cbe830e54cadecbc0c7b4fbaf
Licuatronix <151476512+Licuatronix@users.noreply.github.com>
2025-07-04 00:07:51 -0400

    Update build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
-export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --build-game=hl2 --android=aarch64,host,21 --target=../aarch64 --enable-opus --togles --disable-warns &&
-./waf install --target=client,server,game_shader_dx9 --strip
 
-if [ -e "srceng-mod-launcher/android/lib/arm64-v8a/README.md" ]; then
-	rm srceng-mod-launcher/android/lib/arm64-v8a/README.md
-fi
+git submodule init && git submodule update
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
+unzip android-ndk-r10e-linux-x86_64.zip
+export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
+export NDK_HOME=$PWD/android-ndk-r10e/
+./waf configure -T release --build-game=hl2mp --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,4.9,21 --togles --disable-warns &&
+./waf build
